6e253f7578e219c749ae5ce5c0fdb6c293df02ff
Alexander <github@lichter.io>
2025-02-09 17:39:31 +0100

    chore: style improvements

--- app/app.vue
+++ app/app.vue
@@ -29,22 +29,41 @@ onMounted(() => {
 <template>
   <UApp>
     <div class="p-2 md:p-4 lg:p-8">
-      <h1 class="text-3xl font-bold">
-        LiveVue.dev - Live streams about Vue.js and Nuxt.js
-      </h1>
-      <ColorModeButton />
-      <div class="my-4">
+      <div class="grid grid-cols-12 justify-center items-center">
+        <div></div>
+        <div class="text-center col-span-10">
+          <h1 class="text-3xl font-bold">LiveVue.dev</h1>
+          <h2>Live streams about Vue.js and Nuxt.js</h2>
+        </div>
+        <div class="ml-auto">
+          <ColorModeButton />
+        </div>
+      </div>
+      <div class="my-4 mt-8 flex justify-center">
         <URadioGroup
-        orientation="horizontal"
-        v-model="selectedItem"
-        :items="items"
-        :disabled="status === 'pending'"
-      />
+          size="xl"
+          orientation="horizontal"
+          v-model="selectedItem"
+          :items="items"
+          :disabled="status === 'pending'"
+        />
       </div>
       <!-- TODO: Overlay when loading -->
-      <div class="grid md:grid-cols-2 xl:grid-cols-4 gap-8 my-8">
+      <div class="grid md:grid-cols-2 xl:grid-cols-4 gap-8 my-16">
         <StreamCard v-for="stream in results" :stream />
       </div>
+      <footer class="flex justify-center mt-8">
+        <div>
+          Find the code on
+          <NuxtLink
+            target="_blank"
+            href="https://github.com/TheAlexLichter/livevue.dev"
+            aria-label="GitHub"
+          >
+            <Icon name="lucide:github" />
+          </NuxtLink>
+        </div>
+      </footer>
     </div>
   </UApp>
 </template>
