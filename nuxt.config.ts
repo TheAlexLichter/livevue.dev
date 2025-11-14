b6934b390220119cb6eba11708a6a44e86247a8a
Alexander <github@lichter.io>
2025-11-14 10:43:12 +0100

    chore: move to nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
-  future: {
-    compatibilityVersion: 4
-  },
   runtimeConfig: {
     twitch: {
       clientId: '',
